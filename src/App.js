86490aaa896632c02a1e44cdae6f203cc35ba98f
PascalUlor <pascalulor@yahoo.com>
2020-06-27 08:14:29 +0100

    add nginx to production build

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React NOW!!!
+          DOCKERIZING REACT NOW!!!
         </a>
       </header>
     </div>
